cd38bc57abeeb54e33d55ef622bffcf39bca6d05
wx_yangmin <1611746089@qq.com>
2018-09-05 22:29:04 +0800

    '提交修改ts文件'

--- src/pages/industrydetial/industrydetial.ts
+++ src/pages/industrydetial/industrydetial.ts
@@ -19,8 +19,6 @@ export class IndustrydetialPage {
   public enabled:boolean;//能否上拉加载
   public isIndustry = false;
   public isSkill =false;
-  public isIndustryMore = true;
-  public isSkillMore = true;
   public IndustrySearchData = [];
   public infoSkillMore = [];
   public SearchData = [];
@@ -39,6 +37,7 @@ export class IndustrydetialPage {
   public secondSkillId:number;
   public industryNameChecked:any;
   public skiiNameChecked:any;
+  public type:any;
   constructor(public navCtrl: NavController, public navParams: NavParams,public IndustryMoreData:MamenDataProvider,
     public SkillLabelMoreData:MamenDataProvider,
     public Skilltwolabel:MamenDataProvider,public IndustrySearch:MamenDataProvider,public DetialSearch:MamenDataProvider  ) {
@@ -50,15 +49,25 @@ export class IndustrydetialPage {
       this.skiiNameChecked =name1;
       this.skilldoubleData['fName'] = name1;
       console.log(this.skilldoubleData['fName'],'技能1111');
+
+      this.type = this.navParams.get('type');
+      console.log(this.type,'this.type111');
+      // this.type=''
   }
   ionViewDidLoad() {
     this.getinstudryListData();
     this.getSkillMoreData();
-    this.getSkillCheckd();
+    
     // this.getSecondaryData('');
-    this.getLabelMore();
     //this.doRefresh('');
     this.search = '';
+    this.type = this.navParams.get('type');
+    console.log(this.type,'typetypetypetypetype')
+    if(this.type == 'indeustry') {
+      this.getLabelMore();
+    }else {
+      this.getSkillCheckd();
+    }
   }
   // 行业更多数据
   getLabelMore() {
@@ -66,7 +75,6 @@ export class IndustrydetialPage {
       res=>{
         if(this.industryNameChecked == undefined) {
           this.IndustrydetialArr = res.data.list;
-          console.log(this.IndustrydetialArr,'全部');
         }else {
           for(var i =0;i < res.data.list.length;i++){
             if(res.data.list[i].industryName == this.industryNameChecked){
@@ -78,16 +86,13 @@ export class IndustrydetialPage {
         console.log(error);
       }
     )
-    // this.isSkillMore = false;
   }
   // 技能更多
   getSkillCheckd() {
     this.SkillLabelMoreData.getSkillLabelMoreData(getskillUrl,1,this.pageNum,this.pageSize).subscribe(
       res=>{
-        // console.log(res.data,'1111111111')
         if(this.skiiNameChecked == undefined) {
           this.infoSkillMore = res.data.list;
-          console.log(this.infoSkillMore,'全部技能');
         }else {
           for(var i =0;i < res.data.list.length;i++){
             if(res.data.list[i].fName == this.skiiNameChecked){
@@ -99,9 +104,7 @@ export class IndustrydetialPage {
         console.log(error);
       }
     )
-    // this.isIndustryMore = false;
   }
-
 // 下拉刷新
   // doRefresh(refresher) {
   //   //初始化或下拉刷新时设置默认值
@@ -128,19 +131,14 @@ export class IndustrydetialPage {
   // 上拉加载
   loadMore(loadEvent){
     let newArr = [];
-    console.log(loadEvent+ 'rwerwerwerwrwrwrwr') 
-    console.log(11111 +'=================');
     this.pageNum++;
     this.IndustryMoreData.getIndustryMoreData(getindustryUrl,1,this.pageNum,this.pageSize).subscribe(
         res=>{
           newArr = this.IndustrydetialArr.concat(res.data.list);
         this.IndustrydetialArr = newArr;
         setTimeout(() => {
-          // console.log(22222);
           loadEvent.complete();
-          console.log('加载更多');
         }, 2000);
-        
         /*如果已经是最后一页，则禁止上拉加载*/
           if( res.data.list == 0){
             console.log('加载完');
@@ -159,7 +157,6 @@ export class IndustrydetialPage {
     this.DetialSearch.getDetialSearch(getdetialSearch,search).subscribe(
       res=>{
         this.SearchData = res.data;
-        // console.log(this.SearchData,'================1111');
       },error=>{
         console.log(error);
       }
@@ -170,24 +167,13 @@ export class IndustrydetialPage {
     this.search = search;
     this.getSearchDetail(this.search);
     // console.log(search,'搜索字段');
-    this.isIndustryMore = false;
-    this.isSkillMore =false;
     this.isFiltersearch = false;
     this.isSearch = true;
-    // if(this.SearchData.length <1) {
-    //   this.isIndustryMore = true;
-    // }
   }
   // 行业标签下拉
   downindustryShow() {
     this.isIndustry = !this.isIndustry;
     this.isSkill = false;
-    this.isIndustryMore = false;
-    this.isSkillMore =false;
-    // if(this.isIndustry) {
-    //   this.isIndustryMore = true;
-    // }
-    
   }
   // 获取行业筛选数据
   getSearchadviser(industryId,firstSkillId,secondSkillId) {
@@ -200,13 +186,10 @@ export class IndustrydetialPage {
       }
     )
   }
-
   // 技能下拉
   downSkill() {
     this.isSkill =  !this.isSkill;
     this.isIndustry = false;
-    this.isIndustryMore = false;
-    this.isSkillMore =false;
   }
   //获取行业标签
   getinstudryListData() {
@@ -214,7 +197,6 @@ export class IndustrydetialPage {
     this.IndustryMoreData.getIndustryMoreData(getindustryUrl,2,1,999).subscribe(
         res=>{
           this.IndustryTotal = res.data.list;
-          // console.log(this.IndustryTotal);
         },error=>{
           console.log(error);
         }
@@ -225,13 +207,9 @@ export class IndustrydetialPage {
     this.skillChecked = index;
     this.IndustryName = value;
     this.isIndustry = false;
-    // this.isIndustryMore = false;
     this.getSearchadviser(this.industryId=1,this.firstSkillId=0,this.secondSkillId=0);
-    // console.log(this.IndustryName,'000000000000');
     this.isSearch = false;
     this.isFiltersearch = true;
-    this.isIndustryMore = false;
-    this.isSkillMore =false;
   }
    //获取技能标签
    getSkillMoreData () {
@@ -250,10 +228,7 @@ export class IndustrydetialPage {
   getSecondaryData(id) {
     this.Skilltwolabel.getSkillSecondaryData(getskilltwoUrl,id,0,999).subscribe(
       res=>{
-        // console.log(1111);
-      // console.log(res);
         this.SkillLabelTwoArr = res.data.list;
-        // console.log(this.SkillLabelTwoArr,'二级技能');
       },error=>{
         console.log(error);
       }
@@ -273,14 +248,12 @@ export class IndustrydetialPage {
     if(index == undefined){
       this.skilldoubleData = value;
       this.getSecondaryData('');
-      console.log(this.skilldoubleData,'=======')
     }else if (index !== undefined){
     this.skillChecked = index;
     this.inputValue = ''; 
     this.skillBgcolor = null;
     this.skilldoubleData = value;
     this.getSecondaryData(value.sfid);
-      console.log(this.skilldoubleData,'++++++++++++++++')
     this.isFiltersearch = true;
     this.isSearch = false;
     this.getSearchadviser(this.industryId=0,this.firstSkillId=1,this.secondSkillId=0);
@@ -293,8 +266,6 @@ export class IndustrydetialPage {
     this.isSkill = false;
     this.isSearch = false;
     this.isFiltersearch = true;
-    this.isIndustryMore = false;
-    this.isSkillMore =false;
     this.getSearchadviser(this.industryId=0,this.firstSkillId=1,this.secondSkillId=3);
   }
  }
