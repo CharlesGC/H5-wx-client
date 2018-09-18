2ef39c87593f5853329894e91202147f53bf1143
CharlesGuo <1432997325@qq.com>
2018-09-18 23:03:49 +0800

    bug修改

--- src/pages/my-project/client/project-decument-browser/project-decument-browser.ts
+++ src/pages/my-project/client/project-decument-browser/project-decument-browser.ts
@@ -18,10 +18,10 @@ import { getDocumentDetailUrl } from '../../../../providers/requestUrl';
   templateUrl: 'project-decument-browser.html',
 })
 export class ProjectDecumentBrowserPage {
-  public invoiceType:any;
+  public invoiceType: any;
   public consultantDocumentDetailData = {};
-  constructor(public navCtrl: NavController, public navParams: NavParams,private Provider:MamenDataProvider) {
-    this.invoiceType =0;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private Provider: MamenDataProvider) {
+    this.invoiceType = 0;
   }
 
   ionViewDidLoad() {
@@ -30,38 +30,59 @@ export class ProjectDecumentBrowserPage {
     this.getConsultantDocumentDetails(id);
   }
 
-  getUrlParam(name) {  
+  getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象  
     var r = window.location.search.substr(1).match(reg);  //匹配目标参数  
     if (r != null) {
-        return encodeURI(r[2]);  //返回参数值 
+      return encodeURI(r[2]);  //返回参数值 
     } else {
-        return null; 
+      return null;
     }
- }
+  }
 
-   /*项目支付记录详情数据请求*/
-   getConsultantDocumentDetails(id) {
+  /*项目支付记录详情数据请求*/
+  getConsultantDocumentDetails(id) {
     // let consultantDocumentDetailsUrl = 'http://mamon.yemindream.com/mamon/adviser/getDocumentDetail';
-    const openId = window.sessionStorage.getItem('openId')|| this.getUrlParam('openId');
-    let consultantDocumentDetailsUrl = getDocumentDetailUrl + '?openId=' + openId + '&id='+id;
-    this.Provider.getMamenSwiperData(consultantDocumentDetailsUrl).subscribe(res=>{
-      if(res.code==200) {
-        console.log(res,'--------');
+    const openId = window.sessionStorage.getItem('openId') || this.getUrlParam('openId');
+    let consultantDocumentDetailsUrl = getDocumentDetailUrl + '?openId=' + openId + '&id=' + id;
+    this.Provider.getMamenSwiperData(consultantDocumentDetailsUrl).subscribe(res => {
+      if (res.code == 200) {
+        console.log(res, '--------');
         this.consultantDocumentDetailData = res.data;
-      }else if(res.code == 207) {
+        this.consultantDocumentDetailData['size'] = (this.consultantDocumentDetailData['size'] / 1048576).toPrecision(3)
+
+        if (this.consultantDocumentDetailData['size'] > 1) {
+          this.consultantDocumentDetailData['size'] = this.consultantDocumentDetailData['size'] + ' MB'
+        } else if (this.consultantDocumentDetailData['size'] < 1) {
+          this.consultantDocumentDetailData['size'] = this.consultantDocumentDetailData['size'] * 1024 + ' KB'
+        }
+
+        if (this.consultantDocumentDetailData['format']) {
+          if (this.consultantDocumentDetailData['format'].search(/doc/) !== -1 || this.consultantDocumentDetailData['format'].search(/docx/) !== -1) {
+            this.consultantDocumentDetailData['format'] = 'assets/imgs/' + 'doc.png'
+          } else if (this.consultantDocumentDetailData['format'].search(/ppt/) !== -1 || this.consultantDocumentDetailData['format'].search(/pptx/) !== -1) {
+            this.consultantDocumentDetailData['format'] = 'assets/imgs/' + 'ppt.png'
+          } else if (this.consultantDocumentDetailData['format'].search(/xls/) !== -1 || this.consultantDocumentDetailData['format'].search(/xlsx/) !== -1) {
+            this.consultantDocumentDetailData['format'] = 'assets/imgs/' + 'xls.png'
+          } else if (this.consultantDocumentDetailData['format'].search(/jpg/) !== -1 || this.consultantDocumentDetailData['format'].search(/png/) !== -1 || this.consultantDocumentDetailData['format'].search(/jpeg/) !== -1) {
+            this.consultantDocumentDetailData['format'] = 'assets/imgs/' + 'png.png'
+          } else if (this.consultantDocumentDetailData['format'].search(/pdf/) !== -1) {
+            this.consultantDocumentDetailData['format'] = 'assets/imgs/' + 'pdf.png'
+          }
+        }
+      } else if (res.code == 207) {
         window.localStorage.removeItem('openId');
-      }else{
-        alert('请求出错:'+res.msg);
+      } else {
+        alert('请求出错:' + res.msg);
       }
-    },error=>{
-      console.log('erros===',error);
+    }, error => {
+      console.log('erros===', error);
     })
   }
 
   /*编辑操作*/
   onDocumentEditClick() {
-    this.navCtrl.push(ConsultantInteractionSubmitPage,{data:this.consultantDocumentDetailData});
+    this.navCtrl.push(ConsultantInteractionSubmitPage, { data: this.consultantDocumentDetailData });
   }
 
   /*删除操作*/
