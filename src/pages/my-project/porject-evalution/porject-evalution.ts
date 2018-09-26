92e2c568a1c954da50de0c305ea3475c1c2c7835
CharlesGuo <1432997325@qq.com>
2018-09-26 15:13:37 +0800

    guochao26v3

--- src/pages/my-project/porject-evalution/porject-evalution.ts
+++ src/pages/my-project/porject-evalution/porject-evalution.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MamenDataProvider } from '../../../providers/mamen-data/mamen-data';
-import { projectEvaluateUrl,clientprojectEvaluateUrl } from '../../../providers/requestUrl';
+import { projectEvaluateUrl, clientprojectEvaluateUrl } from '../../../providers/requestUrl';
 
 /**
  * Generated class for the PorjectEvalutionPage page.
@@ -16,9 +16,12 @@ import { projectEvaluateUrl,clientprojectEvaluateUrl } from '../../../providers/
   templateUrl: 'porject-evalution.html',
 })
 export class PorjectEvalutionPage {
-  public evalutionType='';
-  public inputName:any;
-  constructor(public navCtrl: NavController, public navParams: NavParams,private Provider:MamenDataProvider) {
+  public evalutionType = '';
+  public inputName: any;
+  public isEvaluation = false
+  public tipstext: any
+  public isFailed: any
+  constructor(public navCtrl: NavController, public navParams: NavParams, private Provider: MamenDataProvider) {
   }
 
   ionViewDidLoad() {
@@ -30,35 +33,51 @@ export class PorjectEvalutionPage {
   onCancelClick() {
     this.navCtrl.pop();
   }
-
-  getUrlParam(name) {  
+  sureEvaluation() {
+    if (this.isFailed == false) {
+      this.isEvaluation = !this.isEvaluation
+      this.navCtrl.pop()
+    } else {
+      this.isEvaluation = !this.isEvaluation
+      return
+    }
+  }
+  onReturn() {
+    this.isEvaluation = !this.isEvaluation
+    return
+  }
+  getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象  
     var r = window.location.search.substr(1).match(reg);  //匹配目标参数  
     if (r != null) {
-        return encodeURI(r[2]);  //返回参数值 
+      return encodeURI(r[2]);  //返回参数值 
     } else {
-        return null; 
+      return null;
     }
- }
+  }
 
   /*评价请求*/
   onProgramObjectionSubmit(value) {
     let pid = this.navParams.get('pid');
     // let projectDetailsUrl = 'http://mamon.yemindream.com/mamon/customer/getProjectProgramDeatil';
-    const openId = window.sessionStorage.getItem('openId')|| this.getUrlParam('openId');
+    const openId = window.sessionStorage.getItem('openId') || this.getUrlParam('openId');
     let url = this.evalutionType == 'consultantEvalution' ? projectEvaluateUrl : clientprojectEvaluateUrl;
-    let projectDetailsUrl = url + '?openId=' + openId + '&pid='+pid + '&evaluation='+value;
-    this.Provider.getMamenSwiperData(projectDetailsUrl).subscribe(res=>{
-      if(res.code==200) {
-        alert('操作成功！');
-        this.navCtrl.pop();
-      }else if(res.code == 207) {
-        window.localStorage.removeItem('openId');
-      }else{
-        alert('请求出错:'+res.msg);
+    let projectDetailsUrl = url + '?openId=' + openId + '&pid=' + pid + '&evaluation=' + value;
+    this.Provider.getMamenSwiperData(projectDetailsUrl).subscribe(res => {
+      if (res.code == 200) {
+        this.isEvaluation = true
+        this.tipstext = '操作成功'
+        this.isFailed = false
+        //alert('操作成功！');
+        //this.navCtrl.pop();
+      } else {
+        this.isEvaluation = true
+        this.tipstext = '操作失败，请稍后再试'
+        this.isFailed = true
+        //alert('请求出错:' + res.msg);
       }
-    },error=>{
-      console.log('erros===',error);
+    }, error => {
+      console.log('erros===', error);
     })
   }
 
